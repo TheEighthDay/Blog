25c2e35a61f4563f7e5fb5bae5c3ae87d0cde115
1109419614 <1109419614@qq.com>
2017-10-20 23:12:51 +0800

    日记功能

--- blog/blog/forms.py
+++ blog/blog/forms.py
@@ -41,6 +41,21 @@ class AddBlogForm(FlaskForm):
     submit = SubmitField('Submit', render_kw={
         'class': 'btn btn-info',"placeholder": "Submit"},)
 
+class AddDiaryForm(FlaskForm):
+    """增加blog表单"""
+    title = StringField(label="Title", validators=[DataRequired("Please input title")],
+                        render_kw={"required": 'required', "placeholder": "Please input title","class": 'form-control'})
+    time = StringField(label="Time", validators=[DataRequired("Please input time")],
+                render_kw={"required": 'required', "placeholder": "Please input time","class": 'form-control'})
+
+    content = TextAreaField(label='blog', validators=[DataRequired('Please input blog')],
+                            description="Please input blog",
+                            render_kw={"required": "required", "class": "form-control","cols":'60',"rows":'10'})
+    picture = FileField(label="Picture", validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField('Submit', render_kw={
+        'class': 'btn btn-info',"placeholder": "Submit"},)
+
+
 class ContactForm(FlaskForm):
     """联系email表单"""
     email = StringField(label="Your Email", validators=[DataRequired()],
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -8,16 +8,17 @@ sys.setdefaultencoding('utf8')
 from flask import render_template, redirect, flash, url_for, request, abort
 from werkzeug.utils import secure_filename
 from blog import  db, app
-from forms import LoginForm,AddBlogForm,ContactForm
-from models import Blog
+from forms import LoginForm,AddBlogForm,ContactForm,AddDiaryForm
+from models import Blog,Diary
 from Email import send
 from identifycode import gene_code
 
 @app.route('/', methods=['GET'])
 def index():
     """ index """
+    diarys = Diary.query.filter_by(is_valid=True).all()
     blogs = Blog.query.filter_by(is_valid=True).all()
-    return render_template('index.html',blogs=blogs)
+    return render_template('index.html',blogs=blogs,diarys=diarys)
 
 @app.route('/blog/', methods=['GET'])
 def blog():
@@ -59,8 +60,10 @@ def admin():
     global i
     pas=False
     add = AddBlogForm()
+    add2 = AddDiaryForm()
     form=LoginForm()
     blogs = Blog.query.filter_by(is_valid=True).all()
+    diarys = Diary.query.filter_by(is_valid = True).all()
     if (request.method != 'POST'):
          text,image = gene_code()
          i+=1
@@ -77,19 +80,39 @@ def admin():
     if form.validate_on_submit() and identify==text:
             pas = True
     if add.validate_on_submit():
-        pas=True
-        blog = Blog(
-            title = add.data['title'],
-            time = add.data['time'],
-            content =add.data['content'],
-            is_valid = True
-        )
-        db.session.add(blog)
-        db.session.commit()
-        flash('Successful')
+        if(add2.picture.data):
+            pas = True
+            print 'yes'
+            f = add2.picture.data
+            filename = secure_filename(f.filename)
+            print filename
+            addr = 'blog/static/images/diary/' + filename
+            f.save(addr)
+            diary = Diary(
+                title=add2.data['title'],
+                time=add2.data['time'],
+                content=add2.data['content'],
+                is_valid=True,
+                picture=addr
+            )
+            db.session.add(diary)
+            db.session.commit()
+            flash('Successful')
+        else:
+            pas=True
+            blog = Blog(
+                title = add.data['title'],
+                time = add.data['time'],
+                content =add.data['content'],
+                is_valid = True
+            )
+            db.session.add(blog)
+            db.session.commit()
+            flash('Successful')
 
 
-    return render_template('admin.html', form=form, pas=pas, add=add,blogs=blogs,i=str(i))
+
+    return render_template('admin.html', form=form, pas=pas, add=add,blogs=blogs,i=str(i),add2=add2,diarys=diarys)
 
 
 
@@ -104,5 +127,17 @@ def admin_blog_manage(pk):
         return '404'
     blog.is_valid = 0
     db.session.add(blog)
+    db.session.commit()
+    return '201'
+@app.route('/admin/diary/<int:pk>',methods=[ 'POST'])
+def admin_diary_manage(pk):
+    """ blog微博 """
+    if(type(pk)!=int):
+        return '404'
+    diary = Diary.query.filter_by(id=pk, is_valid=1).first()
+    if diary is None:
+        return '404'
+    diary.is_valid = 0
+    db.session.add(diary)
     db.session.commit()
     return '201'
